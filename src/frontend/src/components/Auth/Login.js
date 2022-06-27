e6c9afca4dfd490161e95040ee78e02d30f7b81a
magdalenapetrushevska <magdalenapetrusevska9@gmail.com>
2022-06-27 14:26:50 +0200

    implement login and register with tailwind

--- src/frontend/src/components/Auth/Login.js
+++ src/frontend/src/components/Auth/Login.js
@@ -4,6 +4,7 @@ import { useState } from "react"
 import axios from "axios"
 
 
+
 export default function Login(props){
   const navigate = useNavigate()
   const [username,setUsername] = useState('')
@@ -38,7 +39,7 @@ export default function Login(props){
     return(
         
         <div style={{minHeight:800, marginTop:30}}>
-          <h1>login page</h1>
+       
           <div style={{marginTop:30}}>
           {
             fetchToken()
@@ -46,19 +47,76 @@ export default function Login(props){
               <p>you are logged in</p>
             ):(
                
-                <div>
+                // <div>
 
-                   <form>
-                       <label style={{marginRight: 10 }}>Input Username</label>
-                       <input type='text' onChange={(e)=>setUsername(e.target.value)}/>
+                //    <form>
+                //        <label style={{marginRight: 10 }}>Input Username</label>
+                //        <input type='text' onChange={(e)=>setUsername(e.target.value)}/>
 
-                       <label style={{marginRight: 10 }}>Input Password</label>
-                       <input type='text' onChange={(e)=>setPassword(e.target.value)}/>
+                //        <label style={{marginRight: 10 }}>Input Password</label>
+                //        <input type='text' onChange={(e)=>setPassword(e.target.value)}/>
 
-                       <button type='button' onClick={login}>Login</button>   
-                   </form>
+                //        <button type='button' onClick={login}>Login</button>   
+                //    </form>
 
+                // </div>
+                <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 mt-16">
+                <div className="max-w-md w-full space-y-8">
+                  <div>
+                    <img
+                      className="mx-auto h-12 w-auto"
+                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                      alt="Workflow"
+                    />
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                      Login
+                    </h2>
+                  </div>
+                  <form className="mt-16 space-y-6" >
+                    <div className="rounded-lg shadow-lg -space-y-px">
+                      <div>
+                        <label htmlFor="username" className="sr-only">
+                          Username
+                        </label>
+                        <input
+                          id="username"
+                          name="username"
+                          type="text"
+                          required
+                          className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                          placeholder="Username"
+                          onChange={(e)=>setUsername(e.target.value)}
+                        />
+                      </div>
+                      <div>
+                        <label htmlFor="password" className="sr-only">
+                          Password
+                        </label>
+                        <input
+                          id="password"
+                          name="password"
+                          type="password"
+                          required
+                          className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                          placeholder="Password"
+                          onChange={(e)=>setPassword(e.target.value)}
+                        />
+                      </div>
+      
+                     
+                    </div>
+                    <div>
+                      <button
+                        type="submit"
+                        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={login}>
+                        Login
+                      </button>
+                     
+                    </div>
+                  </form>
                 </div>
+              </div>
             )
           }
           </div>
--- src/frontend/src/components/Auth/Register.js
+++ src/frontend/src/components/Auth/Register.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
+import { LockClosedIcon } from '@heroicons/react/solid'
+
 
 
 const Register = (props) => {
@@ -25,31 +27,90 @@ const Register = (props) => {
         const phone_number = formData.phone_number;
 
         props.onRegister(username,password,phone_number);
-        navigate("/");
+        navigate("/login");
     }
 
 
 
-    return(
-        <>
-         <form onSubmit={onFormSubmit}>
-                       <label style={{marginRight: 10 }}>Username: </label>
-                       <input type='text' id="username"
-                               name="username" onChange={handleChange} />
+    return (
+      <>
+        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 mt-16">
+          <div className="max-w-md w-full space-y-8">
+            <div>
+              <img
+                className="mx-auto h-12 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                alt="Workflow"
+              />
+              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Register
+              </h2>
+            </div>
+            <form className="mt-16 space-y-6" onSubmit={onFormSubmit}>
+              <div className="rounded-lg shadow-lg -space-y-px">
+                <div>
+                  <label htmlFor="username" className="sr-only">
+                    Username
+                  </label>
+                  <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Username"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                </div>
 
-                       <label style={{marginRight: 10 }}>Password: </label>
-                       <input type='text' id="password"
-                               name="password" onChange={handleChange}/>
-
-                        <label style={{marginRight: 10 }}>Phone number: </label>
-                       <input type='text' id="phone_number"
-                               name="phone_number" onChange={handleChange}/>
-
-                       <button id="submit" type="submit" className="btn btn-primary">Submit</button> 
-                   </form>
-
-        </>
-    )
+                <div>
+                  <label htmlFor="phone_number" className="sr-only">
+                    Phone number
+                  </label>
+                  <input
+                    id="phone_number"
+                    name="phone_number"
+                    type="text"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Phone number"
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                    <LockClosedIcon
+                      className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                      aria-hidden="true"
+                    />
+                  </span>
+                  Sign up
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </>
+    );
 }
 
 export default Register;
